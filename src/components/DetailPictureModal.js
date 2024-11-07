dcc148294e77a200890c7c1387dd4abd99798283
Lee Hojun <hppmm@naver.com>
2024-11-08 03:07:26 +0900

    Design detail view

--- src/components/DetailPictureModal.js
+++ src/components/DetailPictureModal.js
@@ -4,6 +4,8 @@ import {useEffect, useState} from "react";
 const DetailPictureModal = ({isOpen, onClose, path}) => {
     const [tags, setTags] = useState([]);
 
+
+
     const getMetadata = () => {
         window.PictureProcessing.requestMetadata(path);
         window.PictureProcessing.responseMetadata((data) => {
@@ -16,16 +18,20 @@ const DetailPictureModal = ({isOpen, onClose, path}) => {
     {
         return null;
     }
-return ReactDOM.createPortal(<div onClick={onClose} className={"fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-10"}>
-    <div onClick={(e) => e.stopPropagation()} className={"bg-black bg-opacity-50 flex h-full w-full rounded-md"}>
-            <div className={"flex rounded overflow-hidden h-full w-full justify-center items-center"}>
-                <img src={`my-scheme-name:///${path}`} className={"w-full h-full"} onLoad={getMetadata}/>
+return ReactDOM.createPortal(
+    <div onClick={onClose} className={"fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-10 backdrop-blur-xl"}>
+    <div className={"flex h-full w-full rounded-md gap-12"}>
+            <div className={"flex h-full w-full justify-center items-center rounded-md overflow-hidden"}>
+
+                    <img src={`my-scheme-name:///${path}`} className={"h-full object-contain"}
+                         onLoad={getMetadata} onClick={(e) => e.stopPropagation()}/>
+
             </div>
         <div className={"w-96 text-white"}>
             {tags}
         </div>
     </div>
-</div>, document.body)
+    </div>, document.body)
 }
 
 export default DetailPictureModal;
